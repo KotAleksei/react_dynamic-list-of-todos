c712626a64cf38a38be6dbf55309c8956691a54b
cherrykot <kot.aleksei43@gmail.com>
2019-07-01 18:16:47 +0300

    add basic logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import './App.css';
 import { getTodos, getUsers } from './api';
+import { ToDoList } from './ToDoList';
 
 
 class  App extends React.Component {
   state = {
-
+    todos: [],
+    users: [],
   }
   async componentDidMount() {
     const [ todos, users ] = await Promise.all([
       getTodos(),
       getUsers(),
     ]);
+    this.setState({
+      todos,
+      users
+    });
   }
   render() {
+    const { todos, users } = this.state;
+    console.log(todos, users);
     return (
       <div className="App">
-          <div>Hello</div>
+          <ToDoList
+            users={users}
+            todos={todos}
+          />
       </div>
     );
   }
