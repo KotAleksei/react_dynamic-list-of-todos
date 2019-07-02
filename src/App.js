0a7e2ec3782598e5f5d36e9c6364f61dd8580e7f
cherrykot <kot.aleksei43@gmail.com>
2019-07-02 10:37:51 +0300

    add button to Load list

--- src/App.js
+++ src/App.js
@@ -2,35 +2,59 @@ import React from 'react';
 import './App.css';
 import { getTodos, getUsers } from './api';
 import { ToDoList } from './ToDoList';
-
+import { Button } from 'semantic-ui-react';
+import { sortTodos } from './sortTodos';
 
 class  App extends React.Component {
   state = {
     todos: [],
-    users: [],
+    load: false,
+    buttonInnerText: 'Load Information',
+    disableButton: false
   }
-  async componentDidMount() {
+  sortedList = (array,sortField) => {
+    this.setState({ todos: sortTodos(array, sortField) })
+  }
+  async loadInformation() {
+    this.setState({
+      disableButton: true,
+      buttonInnerText: "Loading...",
+    });
     const [ todos, users ] = await Promise.all([
       getTodos(),
       getUsers(),
     ]);
+    const fullArr = [ ...todos];
+
+    fullArr.map(el => el.user = users.find(user => user.id === el.userId));
     this.setState({
-      todos,
-      users
+      todos: fullArr,
+      load: true
     });
   }
   render() {
-    const { todos, users } = this.state;
-    console.log(todos, users);
+    const { todos } = this.state;
     return (
       <div className="App">
-          <ToDoList
-            users={users}
-            todos={todos}
-          />
+       {
+         this.state.load ?                   
+        <ToDoList
+          todos={todos}
+          sortTodos={this.sortedList}
+        /> :
+        <Button 
+          color="green"
+          inverted
+          onClick={() => this.loadInformation()}
+          disabled={this.state.disableButton}
+        > 
+          {this.state.buttonInnerText}
+        </Button> 
+       }
       </div>
     );
   }
 }
 
 export default App;
+// <div style={{textAlign: 'center'}}>Loading...</div> 
